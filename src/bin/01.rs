ab5bfe46c43e9ffe7a6cff7ec14ab81aa6409f66
= <umairziyan@protonmail.com>
2024-09-20 11:58:24 +0400

    removed tests

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -90,20 +90,3 @@ fn has_prefixes(line: &str) -> (char, bool) {
     }
     (' ', false)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
